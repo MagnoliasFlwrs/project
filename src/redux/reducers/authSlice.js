98337c59588b1b3fe000eedd3c63371e5194fd42
Savenok Marta <magnolia.sav@gmail.com>
2024-01-09 22:55:26 +0300

    server

--- src/redux/reducers/authSlice.js
+++ src/redux/reducers/authSlice.js
@@ -4,18 +4,35 @@ import axios from 'axios'
 export const authUser = createAsyncThunk(
     'post/authUser',
     async(payload , {rejectWithValue}) => {
-        try {
-            const {user , params} = payload
-            // localStorage.setItem('user', JSON.stringify(user))
-            const res = await axios.post(`http://localhost:8080/users` , user)
-            if (res.status !== 201) {
-                throw new Error('error request')
+        // try {
+        //     const {user , params} = payload
+        //     // localStorage.setItem('user', JSON.stringify(user))
+        //     const res = await axios.post(`http://localhost:8080/user` , user)
+        //     if (res.status !== 201) {
+        //         throw new Error('error request')
+        //     }
+        //     console.log(res.data)
+        //     return res.data
+        // } catch (error) {
+        //     return rejectWithValue(error.message)
+        // }
+        const {user} = payload
+        fetch(`http://localhost:8080/user/?userlogin=${user.userlogin}`).then((res) => {
+            return res.json();
+        }).then((resp) => {
+            //console.log(resp)
+            if (Object.keys(resp).length === 0) {
+                console.log('Please Enter valid username');
+            } else {
+                if (resp.password === user.userpass) {
+                    console.log.success('Success');
+                }else{
+                    console.log('Please Enter valid credentials');
+                }
             }
-            console.log(res.data)
-            return res.data
-        } catch (error) {
-            return rejectWithValue(error.message)
-        }
+        }).catch((err) => {
+            console.log('Login Failed due to :' + err.message);
+        });
     }
 )
 const initialState = {
